47d1a3980d6fee82dcd1b46036dca87c16ee29c8
Catherine Okang-Boye <katieboye44@gmail.com>
2023-02-07 16:02:11 +0000

    toggle fixed

--- javascript/themes.js
+++ javascript/themes.js
@@ -1,30 +1,23 @@
 let theme1 = document.querySelector('#theme_1');
-let theme2 = document.getElementById('theme_2');
-let theme3 = document.getElementById('theme_3');
+let theme2 = document.querySelector('#theme_2');
+let theme3 = document.querySelector('#theme_3');
 let setTheme = document.querySelector('body');
 let togglerPosition = document.querySelector('.header__ball');
 
 
 // Creating function for the different themes.
 theme1.addEventListener('click', () => {
-    setTheme.setAttribute('id', 'theme1');
-    togglerPosition.style.justifyContent = "flex-start";
-    
-    
-    
+    setTheme.setAttribute('id', 'theme1');    
 });
+
 
 theme2.addEventListener('click', () => {
-    setTheme.setAttribute('id', 'theme2');
-    togglerPosition.style.justifyContent = "center";
-    
-    
+    setTheme.setAttribute('id', 'theme2');    
 });
 
+
 theme3.addEventListener('click', () => {
-    setTheme.setAttribute('id', 'theme3');
-    togglerPosition.style.justifyContent = "flex-end";
-    
+    setTheme.setAttribute('id', 'theme3');   
 });
 
 
